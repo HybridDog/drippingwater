110d9bbdb3fb89dc35d2d23fb03d17d51d0b4420
HybridDog <adresse_test@t-online.de>
2015-08-27 12:50:38 +0200

    update code

--- init.lua
+++ init.lua
@@ -3,9 +3,6 @@
 
 -- License of code, textures & sounds: CC0
 
---Random
-math.randomseed(3)
-
 
 --Drop entities
 
@@ -23,26 +20,25 @@ minetest.register_entity("drippingwater:drop_water", {
 
 	on_activate = function(self, staticdata)
 		self.object:setsprite({x=0,y=0}, 1, 1, true)
+		self.object:setacceleration({x=0, y=-5, z=0})
 	end,
 
 	on_step = function(self, dtime)
-	local k = math.random(1,222)
-	local ownpos = self.object:getpos()
+		if self.object:getvelocity().y ~= 0 then
+			return
+		end
 
-	if k==1 then
-	self.object:setacceleration({x=0, y=-5, z=0})
-	end
+		local ownpos = self.object:getpos()
+		ownpos.y = ownpos.y - 0.5
 
-	if minetest.get_node({x=ownpos.x, y=ownpos.y +0.5, z=ownpos.z}).name == "air" then
-	self.object:setacceleration({x=0, y=-5, z=0})
-	end
-
-		if minetest.get_node({x=ownpos.x, y=ownpos.y -0.5, z=ownpos.z}).name ~= "air" then
-		self.object:remove()
-		minetest.sound_play({name="drippingwater_drip"}, {pos = ownpos, gain = 0.5, max_hear_distance = 8})
+		if minetest.get_node(ownpos).name ~= "air" then
+			self.object:remove()
+			ownpos.y = ownpos.y + 0.5
+			minetest.sound_play({name="drippingwater_drip"}, {pos = ownpos, gain = 0.5, max_hear_distance = 8})
 		end
 	end,
 })
+
 
 
 --lava
@@ -59,75 +55,57 @@ minetest.register_entity("drippingwater:drop_lava", {
 
 	on_activate = function(self, staticdata)
 		self.object:setsprite({x=0,y=0}, 1, 0, true)
+		self.object:setacceleration({x=0, y=-5, z=0})
 	end,
 
 	on_step = function(self, dtime)
-	local k = math.random(1,222)
-	local ownpos = self.object:getpos()
+		if self.object:getvelocity().y ~= 0 then
+			return
+		end
 
-	if k==1 then
-	self.object:setacceleration({x=0, y=-5, z=0})
-	end
+		local ownpos = self.object:getpos()
+		ownpos.y = ownpos.y - 0.5
 
-	if minetest.get_node({x=ownpos.x, y=ownpos.y +0.5, z=ownpos.z}).name == "air" then
-	self.object:setacceleration({x=0, y=-5, z=0})
-	end
-
-
-		if minetest.get_node({x=ownpos.x, y=ownpos.y -0.5, z=ownpos.z}).name ~= "air" then
-		self.object:remove()
-		minetest.sound_play({name="drippingwater_lavadrip"}, {pos = ownpos, gain = 0.5, max_hear_distance = 8})
+		if minetest.get_node(ownpos).name ~= "air" then
+			self.object:remove()
+			ownpos.y = ownpos.y + 0.5
+			minetest.sound_play({name="drippingwater_lavadrip"}, {pos = ownpos, gain = 0.5, max_hear_distance = 8})
 		end
 	end,
 })
+
+
+local function spawn_drop(pos, name)
+	if minetest.get_node({x=pos.x, y=pos.y -1, z=pos.z}).name == "air"
+	and minetest.get_node({x=pos.x, y=pos.y -2, z=pos.z}).name == "air" then
+		minetest.add_entity({x=pos.x+(math.random()-0.5)*0.9, y=pos.y-0.5, z=pos.z+(math.random()-0.5)*0.9}, name)
+	end
+end
 
 
 
 --Create drop
 
-minetest.register_abm(
-        {nodenames = {"group:crumbly"},
+minetest.register_abm({
+	nodenames = {"group:crumbly"},
 	neighbors = {"group:water"},
-        interval = 2,
-        chance = 22,
-        action = function(pos)
-		if minetest.get_node({x=pos.x, y=pos.y -1, z=pos.z}).name == "air" and
-		minetest.get_node({x=pos.x, y=pos.y -2, z=pos.z}).name == "air" then
-		local i = math.random(-45,45) / 100
-		minetest.add_entity({x=pos.x + i, y=pos.y - 0.5, z=pos.z + i}, "drippingwater:drop_water")
-		end
-        end,
+	interval = 2,
+	chance = 22,
+	action = function(pos)
+		spawn_drop(pos, "drippingwater:drop_water")
+	end,
 })
 
-
---Cloudstone
-
-minetest.register_abm(
-        {nodenames = {"default:cloud"},
-        interval = 0,
-        chance = 1,
-        action = function(pos)
-		if minetest.get_node({x=pos.x, y=pos.y -1, z=pos.z}).name == "air" and
-		minetest.get_node({x=pos.x, y=pos.y -2, z=pos.z}).name == "air" then
-		local i = math.random(-45,45) / 100
-		minetest.add_entity({x=pos.x + i, y=pos.y - 0.5, z=pos.z + i}, "drippingwater:drop_water")
-		end
-        end,
-})
 
 
 --Create lava drop
 
-minetest.register_abm(
-        {nodenames = {"group:crumbly"},
+minetest.register_abm({
+	nodenames = {"group:crumbly"},
 	neighbors = {"group:lava"},
-        interval = 2,
-        chance = 22,
-        action = function(pos)
-		if minetest.get_node({x=pos.x, y=pos.y -1, z=pos.z}).name == "air" and
-		minetest.get_node({x=pos.x, y=pos.y -2, z=pos.z}).name == "air" then
-		local i = math.random(-45,45) / 100
-		minetest.add_entity({x=pos.x + i, y=pos.y - 0.5, z=pos.z + i}, "drippingwater:drop_lava")
-		end
-        end,
+	interval = 2,
+	chance = 22,
+	action = function(pos)
+		spawn_drop(pos, "drippingwater:drop_lava")
+	end,
 })
